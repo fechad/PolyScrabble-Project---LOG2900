1934e75f7d4fcf7795d90f1aaac8c49fd9eb2ab1
annagx <anna.guo@polymtl.ca>
2022-01-28 10:00:45 -0500

    waiting room from parameters

--- client/src/app/components/game-setup-dialog/game-setup-dialog.component.ts
+++ client/src/app/components/game-setup-dialog/game-setup-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { GamesListService } from '@app/services/games-list.service';
 
 @Component({
@@ -9,8 +10,22 @@ import { GamesListService } from '@app/services/games-list.service';
     styleUrls: ['./game-setup-dialog.component.scss'],
 })
 export class GameSetupDialogComponent implements OnInit {
+    serverMessage: string = '';
+    serverClock: Date;
+
+    wordInput = '';
+    serverValidationResult = '';
+    messageToServer = '';
+
+    broadcastMessage = '';
+    serverMessages: string[] = [];
+
+    roomMessage = '';
+    roomMessages: string[] = [];
+
     gameParametersForm: FormGroup;
     constructor(
+        private router: Router,
         private formBuilder: FormBuilder,
         private dialogRef: MatDialogRef<GameSetupDialogComponent>,
         private gameService: GamesListService,
@@ -28,13 +43,12 @@ export class GameSetupDialogComponent implements OnInit {
         this.dialogRef.close();
     }
 
-    ngOnInit() {
-        // this.gameParametersForm = this.formBuilder.group({
-        //     id: [''],
-        //     playerName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
-        //     timer: new FormControl('', [Validators.required]),
-        //     dictionary: new FormControl('', [Validators.required]),
-        // });
+    get socketId() {
+        return this.gameService.socket.id ? this.gameService.socket.id : '';
+    }
+
+    ngOnInit(): void {
+        this.connect();
     }
 
     onSubmit() {
@@ -57,5 +71,48 @@ export class GameSetupDialogComponent implements OnInit {
         if (selectedMin === '5' && selectedSec === '30') {
             second.selectedIndex = 0;
         }
+    }
+
+    createGame() {
+        const uuid = 1;
+        this.router.navigate(['/game', uuid]);
+    }
+
+    connect() {
+        if (!this.gameService.isSocketAlive()) {
+            this.gameService.connect();
+            this.configureBaseSocketFeatures();
+        }
+    }
+
+    configureBaseSocketFeatures() {
+        this.gameService.on('connect', () => {
+            console.log(`Connexion par WebSocket sur le socket ${this.socketId}`);
+        });
+        // Afficher le message envoyé lors de la connexion avec le serveur
+        this.gameService.on('hello', (message: string) => {
+            this.serverMessage = message;
+        });
+
+        // Afficher le message envoyé à chaque émission de l'événement "clock" du serveur
+        this.gameService.on('clock', (time: Date) => {
+            this.serverClock = time;
+        });
+
+        // Gérer l'événement envoyé par le serveur : afficher le résultat de validation
+        this.gameService.on('wordValidated', (isValid: boolean) => {
+            const validationString = `Le mot est ${isValid ? 'valide' : 'invalide'}`;
+            this.serverValidationResult = validationString;
+        });
+
+        // Gérer l'événement envoyé par le serveur : afficher le message envoyé par un client connecté
+        this.gameService.on('massMessage', (broadcastMessage: string) => {
+            this.serverMessages.push(broadcastMessage);
+        });
+
+        // Gérer l'événement envoyé par le serveur : afficher le message envoyé par un membre de la salle
+        this.gameService.on('roomMessage', (roomMessage: string) => {
+            this.roomMessages.push(roomMessage);
+        });
     }
 }
--- client/src/app/services/games-list.service.ts
+++ client/src/app/services/games-list.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
 import { GameParameters } from '@app/classes/game-parameters';
+import { io, Socket } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GamesListService {
+    socket: Socket;
+    readonly url: string = 'http://localhost:4200';
     private gamesList: GameParameters[] = [];
+
+    isSocketAlive() {
+        return this.socket && this.socket.connected;
+    }
+
+    connect() {
+        this.socket = io(environment.serverUrl, { transports: ['websocket'], upgrade: false });
+    }
+
+    disconnect() {
+        this.socket.disconnect();
+    }
+
+    on<T>(event: string, action: (data: T) => void): void {
+        this.socket.on(event, action);
+    }
+
+    send<T>(event: string, data?: T): void {
+        if (data) {
+            this.socket.emit(event, data);
+        } else {
+            this.socket.emit(event);
+        }
+    }
+
     constructor() {}
 
     addGame(game: GameParameters) {
@@ -20,4 +49,16 @@ export class GamesListService {
     getAllGames() {
         return this.gamesList;
     }
+    // createNewGame(): unknown {
+    //     this.socket.emit('create-game', { creategame: 1 });
+    //     const observable = new Observable<string>((observer: Observer<string>) => {
+    //         this.socket.on('new-game', (data: string) => {
+    //             observer.next(data);
+    //         });
+    //         return () => {
+    //             this.socket.disconnect();
+    //         };
+    //     });
+    //     return observable;
+    // }
 }
