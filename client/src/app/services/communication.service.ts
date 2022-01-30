a0cff92e693f771d0fc0016565ee1e0f95c7281c
Xavier L'Heureux <dev.xlheureux@gmail.com>
2022-01-30 16:33:53 -0500

    Get rooms in client

--- client/src/app/services/communication.service.ts
+++ client/src/app/services/communication.service.ts
@@ -1,17 +1,57 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Message } from '@app/classes/message';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Parameters } from '@app/classes/parameters';
+import { fromEventPattern, Observable, of } from 'rxjs';
+import { catchError, shareReplay, startWith } from 'rxjs/operators';
+import { io, Socket } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
+
+
+export type RoomId = number;
+export type PlayerId = string;
+export type Player = { name: string; id: PlayerId };
+
+export class Room {
+    readonly parameters: Parameters;
+    readonly name: string;
+    readonly id: RoomId;
+    readonly mainPlayer: Player;
+    readonly otherPlayer: Player | undefined;
+}
 
 @Injectable({
     providedIn: 'root',
 })
 export class CommunicationService {
     private readonly baseUrl: string = environment.serverUrl;
+    private readonly roomsSocket: Socket = io(`${environment.socketUrl}/waitingRoom`);
+    //private readonly mainSocket: Socket = io(`${environment.socketUrl}/`);
+    //private gameSocket: Socket | undefined = undefined;
 
-    constructor(private readonly http: HttpClient) {}
+    readonly rooms: Observable<Room[]> = fromEventPattern<Room[]>((handler) => { this.roomsSocket.on('broadcastRooms', handler) }, (handler) => { this.roomsSocket.off('broadcastRooms', handler) }).pipe(
+        startWith(<Room[]>[]),
+        shareReplay(1),
+      );
+    
+
+    constructor(private readonly http: HttpClient) {
+        this.listenRooms();
+        (<any>window)['communication'] = this; // TODO: wack
+        (<any>window)['io'] = io; // TODO: wack
+    }
+
+    listenRooms(): Promise<void> {
+        return new Promise((resolve) => {
+            if (this.roomsSocket.connected) return;
+            this.roomsSocket.once('connect', () => resolve());
+            this.roomsSocket.connect();
+        });
+    }
+
+    unlistenRooms() {
+        if (this.roomsSocket.connected) this.roomsSocket.disconnect();
+    }
 
     basicGet(): Observable<Message> {
         return this.http.get<Message>(`${this.baseUrl}/example`).pipe(catchError(this.handleError<Message>('basicGet')));
