d37ad03644510a71d5a6d4b0538df6de5d658d8b
Etienne123456 <etienneaumais@gmail.com>
2022-01-31 23:03:50 -0500

    ajout d'un timer qui change le tour apres 60 secondes

--- client/src/app/components/infos-box/infos-box.component.ts
+++ client/src/app/components/infos-box/infos-box.component.ts
@@ -1,15 +1,33 @@
+<<<<<<< HEAD
 import { Component, Injectable, Injector, OnInit } from '@angular/core';
 import { SkipTurnService } from '@app/services/skip-turn.service';
+=======
+import { AfterViewInit, Component } from '@angular/core';
+import { ChronoService } from '@app/services/chrono.service';
+import { SkipTurn } from '@app/services/skipTurn.service';
+>>>>>>> f7a4b98... ajout d'un timer qui change le tour apres 60 secondes
 @Component({
     selector: 'app-infos-box',
     templateUrl: './infos-box.component.html',
     styleUrls: ['./infos-box.component.scss'],
 })
+<<<<<<< HEAD
 @Injectable()
 export class InfosBoxComponent implements OnInit {
     constructor(public skipTurn: SkipTurnService) {}
+=======
+export class InfosBoxComponent implements AfterViewInit {
+    constructor(
+        public skipTurn: SkipTurn, 
+        public chronoService:ChronoService
+        ) {}
+>>>>>>> f7a4b98... ajout d'un timer qui change le tour apres 60 secondes
 
-    ngOnInit(): void {}
+    ngAfterViewInit(): void {
+        this.chronoService.startTimer();
+
+
+    }
 }
 Injector.create({
     providers: [
