da44fb19f5f6aab1d6cd868f4a8a33e1c91ea3a3
Etienne123456 <etienneaumais@gmail.com>
2022-02-02 16:25:13 -0500

    boite message implémenter reste a connecter le serveur

--- client/src/app/components/chat-box/chat-box.component.ts
+++ client/src/app/components/chat-box/chat-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-chat-box',
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./chat-box.component.scss'],
 })
 export class ChatBoxComponent implements OnInit {
+    @ViewChild('scroll') private scroller: ElementRef;
+    messages: string[] = [];
+    textValue: string = '';
+    yourMessage: boolean = true;
     constructor() {}
 
     ngOnInit(): void {}
+
+    sendMessage() {
+        this.messages.push(this.textValue);
+        this.textValue = '';
+        this.scrollToBottom();
+    }
+    scrollToBottom() {
+        this.scroller.nativeElement.scrollTop = this.scroller.nativeElement.scrollHeight;
+    }
 }
