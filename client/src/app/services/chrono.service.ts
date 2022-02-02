f10873353f7785eeaa61532ce8971970156a4130
Etienne123456 <etienneaumais@gmail.com>
2022-02-01 21:53:23 -0500

    preparing for a merge request

--- client/src/app/services/chrono.service.ts
+++ client/src/app/services/chrono.service.ts
@@ -3,24 +3,21 @@ import { SkipTurn } from '@app/services/skipTurn.service';
 import { BehaviorSubject, interval } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ChronoService {
-  constructor(
-    public skipTurn: SkipTurn
-    ) {}
-  time:BehaviorSubject<number> = new BehaviorSubject(0);
-  startTimer(){
-    const obs$ = interval(1000);
-    obs$.subscribe((d)=>{
-      if(d!=59){
-      this.time.next(59 - d);}
-      else{
-        this.time.next(0);
-        this.skipTurn.skipTurn();
-        this.startTimer();
-        
-      }
-    })
-  }
+    constructor(public skipTurn: SkipTurn) {}
+    time: BehaviorSubject<number> = new BehaviorSubject(0);
+    startTimer() {
+        const obs$ = interval(1000);
+        obs$.subscribe((d) => {
+            if (d != 59) {
+                this.time.next(59 - d);
+            } else {
+                this.time.next(0);
+                this.skipTurn.skipTurn();
+                this.startTimer();
+            }
+        });
+    }
 }
