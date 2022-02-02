d5b1002cb7bc63584cfe56fd81e5885b40f71f5b
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-02-02 09:24:26 -0500

    tests passent et merge markers removed

--- client/src/app/components/infos-box/infos-box.component.ts
+++ client/src/app/components/infos-box/infos-box.component.ts
@@ -1,37 +1,18 @@
-<<<<<<< HEAD
 import { AfterViewInit, Component, Injectable, Injector } from '@angular/core';
 import { ChronoService } from '@app/services/chrono.service';
 import { SkipTurnService } from '@app/services/skip-turn.service';
 
-=======
-import { AfterViewInit, Component } from '@angular/core';
-import { ChronoService } from '@app/services/chrono.service';
-import { SkipTurn } from '@app/services/skipTurn.service';
->>>>>>> 381553880f1e48587b8eac11a5c81c2bf6a8b0e6
 @Component({
     selector: 'app-infos-box',
     templateUrl: './infos-box.component.html',
     styleUrls: ['./infos-box.component.scss'],
 })
-<<<<<<< HEAD
 @Injectable()
 export class InfosBoxComponent implements AfterViewInit {
     constructor(public skipTurn: SkipTurnService, public chronoService: ChronoService) {}
 
     ngAfterViewInit(): void {
         this.chronoService.startTimer();
-=======
-export class InfosBoxComponent implements AfterViewInit {
-    constructor(
-        public skipTurn: SkipTurn, 
-        public chronoService:ChronoService
-        ) {}
-
-    ngAfterViewInit(): void {
-        this.chronoService.startTimer();
-
-
->>>>>>> 381553880f1e48587b8eac11a5c81c2bf6a8b0e6
     }
 }
 Injector.create({
--- client/src/app/services/skip-turn.service.ts
+++ client/src/app/services/skip-turn.service.ts
@@ -1,33 +1,16 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD:client/src/app/services/skip-turn.service.ts
 import { ChronoService } from './chrono.service';
-=======
-//import { ChronoService } from '@app/services/chrono.service';
-// TODO : Avoir un fichier séparé pour les constantes et ne pas les répéter!
->>>>>>> 381553880f1e48587b8eac11a5c81c2bf6a8b0e6:client/src/app/services/SkipTurn.service.ts
 
 @Injectable({
     providedIn: 'root',
 })
-<<<<<<< HEAD:client/src/app/services/skip-turn.service.ts
 export class SkipTurnService {
-=======
-export class SkipTurn {
-    /*constructor(
-        public chronoService:ChronoService
-        ) {}*/
->>>>>>> 381553880f1e48587b8eac11a5c81c2bf6a8b0e6:client/src/app/services/SkipTurn.service.ts
     isYourTurn = true;
     chrono: ChronoService;
     constructor() {}
 
     skipTurn() {
         this.isYourTurn = !this.isYourTurn;
-<<<<<<< HEAD:client/src/app/services/skip-turn.service.ts
         // this.chrono.startTimer();
-=======
-        //this.chronoService.time.next(0);
-        //this.chronoService.startTimer();
->>>>>>> 381553880f1e48587b8eac11a5c81c2bf6a8b0e6:client/src/app/services/SkipTurn.service.ts
     }
 }
