6dab0e663ac1dd1e0a952cb3929e75ba780a3986
Justin St-Arnaud <justin.st-arnaud@polymtl.ca>
2022-02-02 12:08:29 -0500

    lint

--- client/src/app/components/buttons-options/buttons-options.component.spec.ts
+++ client/src/app/components/buttons-options/buttons-options.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonsOptionsComponent } from './buttons-options.component';
 
 describe('ButtonsOptionsComponent', () => {
-  let component: ButtonsOptionsComponent;
-  let fixture: ComponentFixture<ButtonsOptionsComponent>;
+    let component: ButtonsOptionsComponent;
+    let fixture: ComponentFixture<ButtonsOptionsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ButtonsOptionsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ButtonsOptionsComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonsOptionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ButtonsOptionsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
