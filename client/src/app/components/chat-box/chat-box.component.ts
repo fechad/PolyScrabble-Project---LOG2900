ea17229f1e16a729054aac8676bbd1cd20fb7dad
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-02-03 13:11:11 -0500

    début logique

--- client/src/app/components/chat-box/chat-box.component.ts
+++ client/src/app/components/chat-box/chat-box.component.ts
@@ -15,11 +15,20 @@ export class ChatBoxComponent implements OnInit {
     ngOnInit(): void {}
 
     sendMessage() {
-        this.messages.push(this.textValue);
-        this.textValue = '';
-        this.scrollToBottom();
+        if (this.validSynthax()) {
+            this.messages.push(this.textValue);
+            this.textValue = '';
+            this.scrollToBottom();
+        }
     }
     scrollToBottom() {
         this.scroller.nativeElement.scrollTop = this.scroller.nativeElement.scrollHeight;
+    }
+    validSynthax(): boolean {
+        if (this.textValue === '') {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
