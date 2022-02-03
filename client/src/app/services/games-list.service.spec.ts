dace7ce6808105c0cc26c4b5292aa54c15d60087
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-02-03 18:27:14 -0500

    ready to be merged

---
+++ client/src/app/services/games-list.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { GamesListService } from './games-list.service';
+
+describe('GamesListService', () => {
+    let service: GamesListService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(GamesListService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+});
