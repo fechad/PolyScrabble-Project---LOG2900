440daf8ad18361a5901f091511acf7e3b52248e1
Etienne123456 <etienneaumais@gmail.com>
2022-02-04 09:57:55 -0500

    test server compile

--- server/app/controllers/leaderboard.controller.spec.ts
+++ server/app/controllers/leaderboard.controller.spec.ts
@@ -1,63 +1,63 @@
-import { Application } from '@app/app';
-import { Message } from '@app/message';
-import { LeaderBoardService } from '@app/services/leaderboard.service';
-import { expect } from 'chai';
-import { StatusCodes } from 'http-status-codes';
-import { createStubInstance, SinonStubbedInstance } from 'sinon';
-import * as supertest from 'supertest';
-import { Container } from 'typedi';
+// import { Application } from '@app/app';
+// import { LeaderBoardService } from '@app/services/leaderboard.service';
+// import { expect } from 'chai';
+// import { StatusCodes } from 'http-status-codes';
+// import { createStubInstance, SinonStubbedInstance } from 'sinon';
+// import * as supertest from 'supertest';
+// import { Container } from 'typedi';
+// import { Message } from './leaderboard.controller';
 
-const HTTP_STATUS_OK = StatusCodes.OK;
-const HTTP_STATUS_CREATED = StatusCodes.CREATED;
+// const HTTP_STATUS_OK = StatusCodes.OK;
+// const HTTP_STATUS_CREATED = StatusCodes.CREATED;
 
-describe('LeaderBoardController', () => {
-    const baseMessage = { title: 'Hello world', body: 'anything really' } as Message;
-    let leaderboardService: SinonStubbedInstance<LeaderBoardService>;
-    let expressApp: Express.Application;
+// describe('LeaderBoardController', () => {
+//     const baseMessage = { title: 'Hello world', body: 'anything really' } as Message;
+//     let leaderboardService: SinonStubbedInstance<LeaderBoardService>;
+//     let expressApp: Express.Application;
 
-    beforeEach(async () => {
-        leaderboardService = createStubInstance(LeaderBoardService);
-        leaderboardService.helloWorld.resolves(baseMessage);
-        leaderboardService.about.returns(baseMessage);
-        leaderboardService.getAllMessages.returns([baseMessage, baseMessage]);
-        const app = Container.get(Application);
-        // eslint-disable-next-line dot-notation
-        Object.defineProperty(app['leaderBoardController'], 'leaderboardService', { value: leaderboardService });
-        expressApp = app.app;
-    });
+//     beforeEach(async () => {
+//         leaderboardService = createStubInstance(LeaderBoardService);
+//         leaderboardService.helloWorld.resolves(baseMessage);
+//         leaderboardService.about.returns(baseMessage);
+//         leaderboardService.getAllMessages.returns([baseMessage, baseMessage]);
+//         const app = Container.get(Application);
+//         // eslint-disable-next-line dot-notation
+//         Object.defineProperty(app['leaderBoardController'], 'leaderboardService', { value: leaderboardService });
+//         expressApp = app.app;
+//     });
 
-    it('should return message from leaderboard service on valid get request to root', async () => {
-        return supertest(expressApp)
-            .get('/api/leaderboard')
-            .expect(HTTP_STATUS_OK)
-            .then((response) => {
-                expect(response.body).to.deep.equal(baseMessage);
-            });
-    });
+//     it('should return message from leaderboard service on valid get request to root', async () => {
+//         return supertest(expressApp)
+//             .get('/api/leaderboard')
+//             .expect(HTTP_STATUS_OK)
+//             .then((response) => {
+//                 expect(response.body).to.deep.equal(baseMessage);
+//             });
+//     });
 
-    it('should return message from leaderboard service on valid get request to about route', async () => {
-        const aboutMessage = { ...baseMessage, title: 'About' };
-        leaderboardService.about.returns(aboutMessage);
-        return supertest(expressApp)
-            .get('/api/leaderboard/about')
-            .expect(HTTP_STATUS_OK)
-            .then((response) => {
-                expect(response.body).to.deep.equal(aboutMessage);
-            });
-    });
+//     it('should return message from leaderboard service on valid get request to about route', async () => {
+//         const aboutMessage = { ...baseMessage, title: 'About' };
+//         leaderboardService.about.returns(aboutMessage);
+//         return supertest(expressApp)
+//             .get('/api/leaderboard/about')
+//             .expect(HTTP_STATUS_OK)
+//             .then((response) => {
+//                 expect(response.body).to.deep.equal(aboutMessage);
+//             });
+//     });
 
-    it('should store message in the array on valid post request to /send', async () => {
-        const message: Message = { title: 'Hello', body: 'World' };
-        return supertest(expressApp).post('/api/leaderboard/send').send(message).set('Accept', 'application/json').expect(HTTP_STATUS_CREATED);
-    });
+//     it('should store message in the array on valid post request to /send', async () => {
+//         const message: Message = { title: 'Hello', body: 'World' };
+//         return supertest(expressApp).post('/api/leaderboard/send').send(message).set('Accept', 'application/json').expect(HTTP_STATUS_CREATED);
+//     });
 
-    it('should return an array of messages on valid get request to /all', async () => {
-        leaderboardService.getAllMessages.returns([baseMessage, baseMessage]);
-        return supertest(expressApp)
-            .get('/api/leaderboard/all')
-            .expect(HTTP_STATUS_OK)
-            .then((response) => {
-                expect(response.body).to.deep.equal([baseMessage, baseMessage]);
-            });
-    });
-});
+//     it('should return an array of messages on valid get request to /all', async () => {
+//         leaderboardService.getAllMessages.returns([baseMessage, baseMessage]);
+//         return supertest(expressApp)
+//             .get('/api/leaderboard/all')
+//             .expect(HTTP_STATUS_OK)
+//             .then((response) => {
+//                 expect(response.body).to.deep.equal([baseMessage, baseMessage]);
+//             });
+//     });
+// });
--- server/app/services/leaderboard.service.ts
+++ server/app/services/leaderboard.service.ts
@@ -1,32 +1,32 @@
-import { Message } from '@app/message';
-import { Service } from 'typedi';
+// import { Message } from '@app/message';
+// import { Service } from 'typedi';
 
-@Service()
-export class LeaderBoardService {
-    clientMessages: Message[];
-    constructor() {
-        this.clientMessages = [];
-    }
+// @Service()
+// export class LeaderBoardService {
+//     clientMessages: Message[];
+//     constructor() {
+//         this.clientMessages = [];
+//     }
 
-    about(): Message {
-        return {
-            emitter: 'Basic Server About Page',
-            text: 'Try calling /api/docs to get the documentation',
-        };
-    }
+//     about(): Message {
+//         return {
+//             emitter: 'Basic Server About Page',
+//             text: 'Try calling /api/docs to get the documentation',
+//         };
+//     }
 
-    async helloWorld(): Promise<Message> {
-        return { emitter: 'Hello World', text: 'Time is now' };
-    }
+//     async helloWorld(): Promise<Message> {
+//         return { emitter: 'Hello World', text: 'Time is now' };
+//     }
 
-    // TODO : ceci est à titre d'exemple. À enlever pour la remise
-    storeMessage(message: Message): void {
-        // eslint-disable-next-line no-console
-        console.log(message);
-        this.clientMessages.push(message);
-    }
+//     // TODO : ceci est à titre d'exemple. À enlever pour la remise
+//     storeMessage(message: Message): void {
+//         // eslint-disable-next-line no-console
+//         console.log(message);
+//         this.clientMessages.push(message);
+//     }
 
-    getAllMessages(): Message[] {
-        return this.clientMessages;
-    }
-}
+//     getAllMessages(): Message[] {
+//         return this.clientMessages;
+//     }
+// }
