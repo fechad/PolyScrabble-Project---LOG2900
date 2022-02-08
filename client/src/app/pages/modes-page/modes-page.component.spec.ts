fc16321149925c4c1859a9343b914f640a03292e
Xavier L'Heureux <dev.xlheureux@gmail.com>
2022-02-07 20:55:03 -0500

    Fix frontend tests

--- client/src/app/pages/modes-page/modes-page.component.spec.ts
+++ client/src/app/pages/modes-page/modes-page.component.spec.ts
@@ -1,5 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { ModesPageComponent } from './modes-page.component';
+
+// Mock implements the behavior of open() method
+export class MatDialogMock {
+    open() {
+        return { afterClosed: () => of({}) };
+    }
+}
+
+export class ActivatedRouteMock {
+    snapshot = { url: ['classique'] };
+}
 
 describe('ModesPageComponent', () => {
     let component: ModesPageComponent;
@@ -8,10 +22,11 @@ describe('ModesPageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [ModesPageComponent],
+            providers: [
+                { provide: MatDialog, useValue: MatDialogMock },
+                { provide: ActivatedRoute, useValue: new ActivatedRouteMock() },
+            ],
         }).compileComponents();
-    });
-
-    beforeEach(() => {
         fixture = TestBed.createComponent(ModesPageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
