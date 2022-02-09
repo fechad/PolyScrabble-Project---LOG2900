510defc39d20c79373ffdfcd6ea864e796b1f80e
Justin St-Arnaud <justin.st-arnaud@polymtl.ca>
2022-02-09 18:49:10 -0500

    word validation service switched into dictionnary service

--- server/app/services/dictionnary.service.ts
+++ server/app/services/dictionnary.service.ts
@@ -8,24 +8,26 @@ export type DictionnaryInfo = { id: number; name: string; words: string[] };
 export class DictionnaryService {
     private dictionnaries: Dictionnary[] = [];
 
-    // constructor() {
-    //     this.dictionnaries.push(
-    //         promises.readFile('./assets/dictionnary.json').then((data) => {
-    //             if (data === null) throw Error('Could not read dictionnary file');
-    //             return { id: 0, name: 'français', words: JSON.parse(data.toString()).words };
-    //         }),
-    //     );
-    // }
     async init() {
         const fileBuffer = await this.readFile('./assets/dictionnary.json');
         const readDicitonnary = JSON.parse(fileBuffer.toString());
-        this.dictionnaries.push(readDicitonnary.words);
+        this.dictionnaries.push({ id: 0, name: 'français', words: readDicitonnary.words });
     }
+
     async readFile(path: string) {
         return await promises.readFile(path);
     }
 
     getDictionnaries() {
         return this.dictionnaries.map((dict) => ({ id: dict.id, name: dict.name } as DictionnaryInfo));
+    }
+    isValidWord(playedWord: string) {
+        if (!this.isWord(playedWord)) {
+            return false;
+        }
+        return this.dictionnaries[0].words.includes(playedWord.toLowerCase());
+    }
+    private isWord(expression: string): boolean {
+        return expression.match(/[a-z]+/i) !== null;
     }
 }
