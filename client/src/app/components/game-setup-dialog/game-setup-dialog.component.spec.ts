7ca9a74f3bb93e71689f326622a8fac006e197a8
annagx <anna.guo@polymtl.ca>
2022-02-12 16:03:26 -0500

    tests for game-setup dialog component

--- client/src/app/components/game-setup-dialog/game-setup-dialog.component.spec.ts
+++ client/src/app/components/game-setup-dialog/game-setup-dialog.component.spec.ts
@@ -1,35 +1,39 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule, routes } from '@app/modules/app-routing.module';
 import { GameSetupDialogComponent } from './game-setup-dialog.component';
 
-export class MatDialogRefMock {
-    close() {
-        return { afterClosed: () => of({}) };
-    }
-}
+const dialogMock = {
+    close: () => {},
+};
 
 describe('GameSetupDialogComponent', () => {
     let component: GameSetupDialogComponent;
     let fixture: ComponentFixture<GameSetupDialogComponent>;
-    let router: Router;
-    let formBuilder: FormBuilder;
-    let matDialogData: typeof MAT_DIALOG_DATA;
+    let router: jasmine.SpyObj<Router>;
 
     beforeEach(async () => {
+        router = jasmine.createSpyObj('Router', ['navigate']);
         await TestBed.configureTestingModule({
             declarations: [GameSetupDialogComponent],
             providers: [
                 { provide: Router, useValue: router },
-                { provide: FormBuilder, useClass: formBuilder },
-                { provide: MatDialogRef, useValue: MatDialogRefMock },
-                { provide: MAT_DIALOG_DATA, useValue: matDialogData },
+                { provide: FormBuilder, useClass: FormBuilder },
+                { provide: MatDialogRef, useValue: dialogMock },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
             ],
-            imports: [HttpClientTestingModule],
+            imports: [RouterTestingModule.withRoutes(routes), HttpClientModule, AppRoutingModule, MatCardModule, MatToolbarModule, MatIconModule],
         }).compileComponents();
+
+        router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
     });
 
     beforeEach(() => {
@@ -38,7 +42,8 @@ describe('GameSetupDialogComponent', () => {
         component.gameParametersForm = new FormGroup({
             id: new FormControl(''),
             playerName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]*$')]),
-            timer: new FormControl(''),
+            minutes: new FormControl('', [Validators.required]),
+            seconds: new FormControl('', [Validators.required]),
             dictionary: new FormControl(''),
         });
 
@@ -50,11 +55,56 @@ describe('GameSetupDialogComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    // it('click on closing button should call close on dialog', fakeAsync(() => {
-    //     const closeDialogSpy = spyOn(component.dialogRef, 'close');
-    //     const button = fixture.debugElement.query(By.css('.icone-cancel'));
-    //     button.nativeElement.click();
-    //     tick();
-    //     expect(closeDialogSpy).toHaveBeenCalled();
-    // }));
+    it('click on closing button should call close on dialog', fakeAsync(() => {
+        const closeDialogSpy = spyOn(component.dialogRef, 'close');
+        const button = fixture.debugElement.query(By.css('.icone-cancel'));
+        button.nativeElement.click();
+        tick();
+        expect(closeDialogSpy).toHaveBeenCalled();
+    }));
+
+    it('onSubmit should not call close on dialog when form invalid', () => {
+        const dialogSpy = spyOn(component.dialogRef, 'close');
+        component.onSubmit();
+        expect(dialogSpy).not.toHaveBeenCalled();
+    });
+
+    it('form invalid when not all required inputs have data', () => {
+        expect(component.gameParametersForm.valid).toBeFalsy();
+    });
+
+    it('form valid when player sets name', () => {
+        const playerName = component.gameParametersForm.controls.playerName;
+        playerName.setValue('Test');
+        expect(component.gameParametersForm.valid).toBeTruthy();
+    });
+
+    it('when form is valid dialog should close', async () => {
+        const playerName = component.gameParametersForm.controls.playerName;
+        playerName.setValue('Test');
+        const spyOnCreateRoom = spyOn(component.communicationService, 'createRoom');
+        const closeDialogSpy = spyOn(component.dialogRef, 'close');
+        await component.onSubmit();
+        expect(spyOnCreateRoom).toHaveBeenCalled();
+        expect(closeDialogSpy).toHaveBeenCalled();
+    });
+
+    it('when form is valid player should be redirected to waiting-room', async () => {
+        const playerName = component.gameParametersForm.controls.playerName;
+        playerName.setValue('Test');
+        const spyOnCreateRoom = spyOn(component.communicationService, 'createRoom');
+        expect(component.gameParametersForm.valid).toBeTruthy();
+        await component.onSubmit();
+        expect(spyOnCreateRoom).toHaveBeenCalled();
+        expect(router.navigate).toHaveBeenCalled();
+    });
+
+    // TO-DO: test console.error line
+    // it('should console error when error found', async () => {
+    //     const spyOnCreateRoom = spyOn(component.communicationService, 'createRoom');
+    //     expect(spyOnCreateRoom).toHaveBeenCalled();
+    //     const err = new Error();
+    //     await component.onSubmit();
+    //     expect(console.error).toHaveBeenCalledWith('Some error');
+    // });
 });
