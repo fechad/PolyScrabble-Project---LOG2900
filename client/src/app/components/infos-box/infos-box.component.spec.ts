c5e2c0de2125f6e7e51682bbb1d424304a0e078a
annagx <anna.guo@polymtl.ca>
2022-02-15 23:22:08 -0500

    ready to merge with dev

--- client/src/app/components/infos-box/infos-box.component.spec.ts
+++ client/src/app/components/infos-box/infos-box.component.spec.ts
@@ -14,19 +14,8 @@ class GameContextServiceMock {
 }
 
 describe('InfosBoxComponent', () => {
-    // let component: InfosBoxComponent;
     let fixture: ComponentFixture<InfosBoxComponent>;
     let communicationServiceSpy: jasmine.SpyObj<CommunicationService>;
-    // let gameContextServiceSpy: jasmine.SpyObj<GameContextService>;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // const gameService = new GameContextServiceMock();
-=======
-    const gameService = new GameContextServiceMock();
->>>>>>> 593b6be... mock for communication service
-=======
-    // const gameService = new GameContextServiceMock();
->>>>>>> 3f28bbd... join setup dialog tests
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [InfosBoxComponent],
@@ -40,32 +29,7 @@ describe('InfosBoxComponent', () => {
     beforeEach(() => {
         communicationServiceSpy = jasmine.createSpyObj('CommunicationService', ['getId']);
         fixture = TestBed.createComponent(InfosBoxComponent);
-        // component = fixture.componentInstance;
+
         fixture.detectChanges();
     });
-
-    // TO-DO
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3f28bbd... join setup dialog tests
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
-    //     gameService.getId();
-    // });
-<<<<<<< HEAD
-=======
-    fit('should create', () => {
-        expect(component).toBeTruthy();
-        gameService.getId();
-    });
->>>>>>> 593b6be... mock for communication service
-=======
->>>>>>> 3f28bbd... join setup dialog tests
-
-    // it('should call reset', () => {
-    //     const resetSpy = spyOn(component, 'reset');
-    //     component.ngAfterViewInit();
-    //     expect(resetSpy).toHaveBeenCalled();
-    // });
 });
