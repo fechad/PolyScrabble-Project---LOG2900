19fe9b668cb65f2ede45a03c2f1c4c3fa5039154
Etienne123456 <etienneaumais@gmail.com>
2022-02-25 17:00:27 -0500

    placing letter on the view, logic is left

--- client/src/app/services/mouse.service.ts
+++ client/src/app/services/mouse.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import { MouseButton } from '@app/components/play-area/play-area.component';
+import { GridService } from './grid.service';
 
 // TODO : Avoir un fichier séparé pour les constantes et ne pas les répéter!
 
@@ -11,10 +12,29 @@ import { MouseButton } from '@app/components/play-area/play-area.component';
 export class MouseService {
     mousePosition: Vec2 = { x: 0, y: 0 };
     buttonPressed = '';
+    prevPos: Vec2 = { x: 0, y: 0 };
+    isHorizontal = true;
+    writingAllowed = false;
+    enter = false;
+    constructor(public gridService: GridService) {}
 
     mouseHitDetect(event: MouseEvent) {
         if (event.button === MouseButton.Left) {
-            this.mousePosition = { x: event.offsetX, y: event.offsetY };
+            this.prevPos = this.mousePosition;
+            this.mousePosition = {
+                x: Math.ceil(((event.offsetX * 500) / 615 - 15) / 33) * 33 + 15,
+                y: Math.ceil(((event.offsetY * 500) / 615 - 5) / 33) * 33 + 5,
+            };
+            if (this.mousePosition.x >= 18 && this.mousePosition.y >= 18) {
+                this.gridService.drawArrow(this.mousePosition.x, this.mousePosition.y, true);
+                this.writingAllowed = true;
+                this.enter = false;
+            }
+            if (this.prevPos.x === this.mousePosition.x && this.prevPos.y === this.mousePosition.y) {
+                this.isHorizontal = !this.isHorizontal;
+                this.gridService.drawArrow(this.mousePosition.x, this.mousePosition.y, this.isHorizontal);
+                this.enter = false;
+            }
         }
     }
 }
