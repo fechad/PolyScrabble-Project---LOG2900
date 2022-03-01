c2203aef611d753a13229b4b4ade358247575373
annagx <anna.guo@polymtl.ca>
2022-03-01 11:55:45 -0500

    select letters and hide menu when no selection

--- client/src/app/components/letter-rack/letter-rack.component.ts
+++ client/src/app/components/letter-rack/letter-rack.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { Letter } from '@app/classes/letter';
 import { GameContextService } from '@app/services/game-context.service';
 import { GridService } from '@app/services/grid.service';
@@ -9,33 +9,45 @@ import { GridService } from '@app/services/grid.service';
     templateUrl: './letter-rack.component.html',
     styleUrls: ['./letter-rack.component.scss'],
 })
-export class LetterRackComponent implements OnInit {
+export class LetterRackComponent implements OnInit, AfterViewInit {
     letters: Letter[];
     timeOut: number;
-    constructor(public gameContextService: GameContextService, public gridService: GridService) {}
+    constructor(public gameContextService: GameContextService, public gridService: GridService, private elementRef: ElementRef) {}
 
     ngOnInit(): void {
         this.gameContextService.rack.subscribe((newRack) => {
             this.letters = newRack;
         });
     }
+
+    ngAfterViewInit() {
+        this.elementRef.nativeElement.querySelector('#letter-container')?.addEventListener('contextmenu', this.menu.bind(this));
+    }
     hideMenu() {
         const menu = document.getElementById('menu') as HTMLElement;
         menu.style.display = 'none';
     }
 
-    menu(): void {
-        window.addEventListener('contextmenu', (event) => {
-            event.preventDefault();
-            const menu = document.getElementById('menu') as HTMLElement;
-            const letter = event.target as HTMLElement;
-            if (menu.style.display === 'block') {
-                this.hideMenu();
-                letter.removeAttribute('id');
-            } else {
-                menu.style.display = 'block';
-                letter.setAttribute('id', 'selected');
+    menu(event: Event): void {
+        event.preventDefault();
+        const menu = document.getElementById('menu') as HTMLElement;
+        const letter = event.target as HTMLElement;
+
+        if (letter.parentElement?.parentElement?.id === 'selected') {
+            letter.parentElement?.parentElement?.removeAttribute('id');
+        } else {
+            menu.style.display = 'block';
+            letter.parentElement?.parentElement?.setAttribute('id', 'selected');
+        }
+        let itemSelected = false;
+
+        const container = document.getElementsByClassName('letter-container');
+        Array.from(container).forEach((letters) => {
+            if (letters.id === 'selected') {
+                itemSelected = true;
             }
         });
+
+        if (!itemSelected) this.hideMenu();
     }
 }
