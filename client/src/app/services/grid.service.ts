321126e700bf76c9da8a1df8aa07081488f9f064
Etienne123456 <etienneaumais@gmail.com>
2022-03-10 00:33:56 -0500

    removed drawTempTileFunction

--- client/src/app/services/grid.service.ts
+++ client/src/app/services/grid.service.ts
@@ -111,21 +111,6 @@ export class GridService {
             this.drawMessage(tile.name, canvasX + AJUST_TILE_X, canvasY + AJUST_TILE_Y, TILE_SIZE);
         }
     }
-    drawTempTiles(letter: string, canvasX: number, canvasY: number) {
-        this.gridContext.beginPath();
-        this.gridContext.rect(
-            (squareSize + offset) * Math.ceil(canvasX / (100 / 3) - 2) + gridOrigin,
-            (squareSize + offset) * Math.ceil(canvasY / (100 / 3) - 2) + gridOrigin,
-            squareSize,
-            squareSize,
-        );
-        this.gridContext.fillStyle = 'burlywood';
-        this.gridContext.fill();
-        this.gridContext.lineWidth = 2.5;
-        this.gridContext.strokeStyle = '#fff';
-        this.gridContext.stroke();
-        this.drawMessage(letter.toUpperCase(), canvasX - squareSize * 0.7, canvasY + AJUST_TILE_Y, TILE_SIZE);
-    }
 
     tempUpdateBoard(lettersToAdd: string, verticalIndex: number, horizontalIndex: number, isHorizontalPlacement: boolean | undefined) {
         const iterationPosition = isHorizontalPlacement ? horizontalIndex : verticalIndex;
