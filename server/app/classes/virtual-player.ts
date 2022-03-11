d983a2d34b0b44f716e15a2f40eabda84b843a67
francois-tourigny <francois.tourigny@polymtl.ca>
2022-03-11 02:00:59 -0500

    game solo avec play turn de connecte

--- server/app/classes/virtual-player.ts
+++ server/app/classes/virtual-player.ts
@@ -2,8 +2,10 @@ import { Letter } from '@app/letter';
 import { Board } from './board';
 import { Game } from './game';
 
-const AI_ID = 1;
-const THRESHOLD = 0.5;
+const AI_ID = 'VP';
+const PROBABILITY = 10;
+// const THRESHOLD = 0.5;
+const DELAY_CHECK_TURN = 1000; // ms
 
 export class VirtualPlayer {
     board: Board;
@@ -14,19 +16,32 @@ export class VirtualPlayer {
     }
 
     playTurn() {
-        const random = Math.floor(Math.random() * 10);
+        const random = Math.floor(Math.random() * PROBABILITY);
         if (this.isBeginner && random === 0) {
-            this.game.skipTurn(AI_ID.toString()); // to test
+            // this.game.skipTurn(AI_ID); // to test
+            this.game.message({ emitter: AI_ID, text: 'I want to skip my turn' });
         } else if (this.isBeginner && random === 1) {
-            let list = '';
+            /* let list = '';
             this.myRack.map((letter) => {
                 if (Math.random() >= THRESHOLD) {
                     list += letter.name.toLowerCase();
                 }
             });
-            this.game.changeLetters(list, AI_ID.toString());
+            this.game.changeLetters(list, AI_ID);*/
+            this.game.message({ emitter: AI_ID, text: 'I want to exchange letters' });
         } else {
-            this.game.message({ emitter: AI_ID.toString(), text: 'I want to place some letters' });
+            this.game.message({ emitter: AI_ID, text: 'I want to place some letters' });
         }
+        // temporaire en attendant implementation placer lettre AI
+        this.game.skipTurn(AI_ID);
+        this.waitForTurn();
+    }
+
+    waitForTurn() {
+        setInterval(() => {
+            if (this.game.getCurrentPlayer().id === AI_ID) {
+                this.playTurn();
+            }
+        }, DELAY_CHECK_TURN);
     }
 }
