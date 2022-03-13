a5d9ad40af9fcb2efa79a7af97eeb16fd0058e76
francois-tourigny <francois.tourigny@polymtl.ca>
2022-03-13 17:46:01 -0400

    validation crossword optimisation possible

--- server/app/classes/joueur-virtuel.ts
+++ server/app/classes/joueur-virtuel.ts
@@ -16,7 +16,7 @@ export class JoueurVirtuel {
 
     playTurn() {
         const random = Math.floor(Math.random() * 10);
-        if(this.isDebutant && random === 0) {
+        if (this.isDebutant && random === 0) {
             // skipTurn
         } else if (this.isDebutant && random === 1) {
             // echangerLettres
@@ -44,33 +44,59 @@ export class JoueurVirtuel {
                 if (valid) arrayPos.push([i, j, false, positions[i][j][1]]);
             }
         }
-        this.validateCrosswords(arrayPos);
-        console.log(arrayPos);
+        const result = this.validateCrosswords(arrayPos);
+        console.log(result);
+        console.log(this.rackToString());
     }
 
     private validateCrosswords(array: any[][]): any[][] {
-        // let validWords: any[][] = [];
+        let validWords: any[][] = [];
+        let starRemains = false;
         for (const option of array) {
             let letterCount = 0;
-            // let replacements: string[] = [];
-            for (let letter of option[3]) {
-                if (letter === '*') {
-                    const row = option[2] ? option[0] : option[0] + letterCount;
-                    const col = option[2] ? option[1] + letterCount : option[1];
-                    const crossWord = this.board.wordGetter.getStringPositionVirtualPlayer(row, col, !option[2]);
-                    console.log(row, !option[2], col, crossWord);
-                    for (const rackLetter of this.myRack) {
-                        // verify if replace only the first *
-                        const attemptedWord = crossWord.replace('*', rackLetter.name.toLowerCase());
-                        if (this.board.dictionnary.isValidWord(attemptedWord)) {
-                            console.log(attemptedWord);
-                            // TODO
-                        }
+            let oneStar = false;
+            const triedWord: string = option[3];
+            let availableLetters = this.rackToString();
+            for (const letter of triedWord) {
+                if (letter.toUpperCase() === letter) {
+                    for (const rackLetter of availableLetters) {
+                        if (rackLetter === letter) availableLetters = availableLetters.replace(rackLetter, '');
                     }
+                }
+                if (letter === '*') {
+                    if (oneStar) starRemains = true;
+                    const replacementOptions = this.starReplacement(option, letterCount, availableLetters, triedWord);
+                    validWords = validWords.concat(replacementOptions);
+                    if (replacementOptions.length === 0 && !oneStar) validWords.push([option[0], option[1], option[2], option[3].slice(0, letterCount)]);
+                    oneStar = true;
                 }
                 letterCount++;
             }
+            if (!oneStar) validWords.push(option);
         }
-        return [[0]];
+        return starRemains ? this.validateCrosswords(validWords) : validWords;
+    }
+
+    starReplacement(option: any[], letterCount: number, availableLetters: string, triedWord: string) {
+        let validWords: any[][] = [];
+        const row = option[2] ? option[0] : option[0] + letterCount;
+        const col = option[2] ? option[1] + letterCount : option[1];
+        const crossWord = this.board.wordGetter.getStringPositionVirtualPlayer(row, col, !option[2]);
+        //par prog dynamique se rappeler des permutations possibles
+        for (const rackLetter of availableLetters) {
+            const attemptedCrossword = crossWord.replace('*', rackLetter.toLowerCase());
+            if (this.board.dictionnary.isValidWord(attemptedCrossword)) {
+                validWords.push([option[0], option[1], option[2], triedWord.replace('*', rackLetter)]);
+            }
+        }
+        return validWords;
+    }
+
+    rackToString(): string {
+        let string = '';
+        for (const letter of this.myRack) {
+            string += letter.name;
+        }
+        return string;
     }
 }
