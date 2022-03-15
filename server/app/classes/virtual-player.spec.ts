be42bdb393b50429ff3c9dd12d5c9a6723923f4e
francois-tourigny <francois.tourigny@polymtl.ca>
2022-03-14 20:06:50 -0400

    utilisation objets et fin get positions

--- server/app/classes/virtual-player.spec.ts
+++ server/app/classes/virtual-player.spec.ts
@@ -1,10 +1,11 @@
 import { Difficulty, GameType, Parameters } from '@app/classes/parameters';
+import { Letter } from '@app/letter';
 import { DictionnaryService } from '@app/services/dictionnary.service';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import { Game } from './game';
 import { Room } from './room';
-import { VirtualPlayer } from './virtual-player';
+import { PlacementOption, VirtualPlayer } from './virtual-player';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 
 describe('VirtualPlayer', () => {
@@ -12,6 +13,11 @@ describe('VirtualPlayer', () => {
     let vP: VirtualPlayer;
     let dictionnaryService: DictionnaryService;
     let parameters: Parameters;
+
+    before(async() => {
+        dictionnaryService = new DictionnaryService();
+        await dictionnaryService.init();
+    });
 
     beforeEach(() => {
         parameters = new Parameters();
@@ -58,4 +64,41 @@ describe('VirtualPlayer', () => {
             done();
         }, timeout);
     });
+
+    it('should give the rack as a string', () => {
+        vP = new VirtualPlayer(true, game);
+        const letterA: Letter = {id: 0, name: 'A', score: 1, quantity: 1};
+        const letterR: Letter = {id: 0, name: 'R', score: 1, quantity: 1};
+        const letterZ: Letter = {id: 0, name: 'Z', score: 1, quantity: 1};
+
+        let testRack: Letter[] = [letterA, letterR];
+        let expectedString = 'AR';
+        game.reserve.letterRacks[1] = testRack;
+        expect(vP['rackToString']()).to.deep.equal(expectedString);
+
+        testRack = [letterZ, letterA, letterR, letterA];
+        expectedString = 'ZARA';
+        game.reserve.letterRacks[1] = testRack;
+        expect(vP['rackToString']()).to.deep.equal(expectedString);
+    });
+
+    it('should find replacement for the contactchar', () => {
+        game.board.board[7][6].setLetter('a');
+        game.board.board[7][7].setLetter('s');
+        vP = new VirtualPlayer(true, game);
+
+        let exploredOptions: PlacementOption[] = [];
+        let option = {row: 2, col: 5, isHorizontal: false, word: '     * '};
+        let letterCount = 5;
+        let availableLetters = 'RZA';
+        let result = vP['contactReplacement'](exploredOptions, option, letterCount, availableLetters);
+        console.log(result);
+    });
+
+    it('randomtest', ()=> {
+        game.board.board[7][6].setLetter('a');
+        game.board.board[7][7].setLetter('s');
+        vP = new VirtualPlayer(true, game);
+        vP.getPlayablePositions();
+    })
 });
--- server/app/classes/virtual-player.ts
+++ server/app/classes/virtual-player.ts
@@ -1,18 +1,23 @@
-import { Letter } from '@app/letter';
 import { Board } from './board';
 import { Game } from './game';
 
 const AI_ID = 'VP';
+const AI_GAME_INDEX = 1;
 const PROBABILITY = 10;
+const BOARD_LENGTH = 15;
+const CONTACT_CHAR = '*';
 // const THRESHOLD = 0.5;
 const DELAY_CHECK_TURN = 1000; // ms
 
+export type PlacementOption = {row: number, col: number, isHorizontal: boolean, word: string};
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export class VirtualPlayer {
     board: Board;
-    myRack: Letter[];
+    
     constructor(readonly isBeginner: boolean, private game: Game) {
         this.board = game.board;
-        this.myRack = game.reserve.letterRacks[AI_ID];
     }
 
     playTurn() {
@@ -43,5 +48,99 @@ export class VirtualPlayer {
                 this.playTurn();
             }
         }, DELAY_CHECK_TURN);
+    }
+
+    getPlayablePositions() {
+        const positions = this.board.getPlayablePositions(this.game.reserve.letterRacks[AI_GAME_INDEX].length);
+        const arrayPos: PlacementOption[] = [];
+        for (let i = 0; i < BOARD_LENGTH; i++) {
+            for (let j = 0; j < BOARD_LENGTH; j++) {
+                // pour chaque orientation
+                for (let k = 0; k < 2; k++) {
+                    let valid = false;
+                    for (const char of positions[i][j][k]) {
+                        if (char !== ' ') valid = true;
+                    }
+                    if (valid) arrayPos.push({row: i, col: j, isHorizontal: k === 0, word: positions[i][j][k]});
+                }
+            }
+        }
+        const result = this.validateCrosswords(arrayPos);
+        console.log(result);
+        console.log(this.rackToString());
+    }
+
+    private validateCrosswords(array: PlacementOption[], exploredOptions: PlacementOption[] = []): PlacementOption[] {
+        let validWords: PlacementOption[] = [];
+        let starRemains = false;
+        for (const option of array) {
+            let letterCount = 0;
+            let oneContact = false;
+            let availableLetters = this.rackToString();
+            for (const letter of option.word) {
+                if (letter.toUpperCase() === letter) {
+                    availableLetters = availableLetters.replace(letter, '');
+                } 
+                if (letter === CONTACT_CHAR) {
+                    if (oneContact) {
+                        starRemains = true; 
+                        break;
+                    }
+                    oneContact = true;
+                    const replacementOptions = this.contactReplacement(exploredOptions, option, letterCount, availableLetters);
+                    validWords = validWords.concat(replacementOptions);
+                    if (replacementOptions.length === 0) validWords.push(this.deepCopyPlacementOption(option, option.word.slice(0, letterCount)));
+                }
+                letterCount++;
+            }
+            if (!oneContact) validWords.push(option);
+        }
+        return starRemains ? this.validateCrosswords(validWords, exploredOptions) : validWords;
+    }
+
+    private contactReplacement(exploredOptions: PlacementOption[], option: PlacementOption, letterCount: number, availableLetters: string): PlacementOption[] {
+        const row = option.isHorizontal ? option.row : option.row + letterCount;
+        const col = option.isHorizontal ? option.col + letterCount : option.col;
+        let alreadyFound = false;
+        const validWords: PlacementOption[] = [];
+        for (const explored of exploredOptions) {
+            if (explored.row === row && explored.col === col && explored.isHorizontal === option.isHorizontal) {
+                for (const solution of explored.word) {
+                    validWords.push(this.deepCopyPlacementOption(option, option.word.replace(CONTACT_CHAR, solution)));
+                }
+                alreadyFound = true;
+                break;
+            }
+        }
+        if (!alreadyFound) {
+            const crossWord = this.board.wordGetter.getStringPositionVirtualPlayer(row, col, !option.isHorizontal);
+            let possibleLetters = '';
+            let alreadyIn = false;
+            for (const rackLetter of availableLetters) {
+                for (let letter of possibleLetters) if (letter === rackLetter) alreadyIn = true;
+                if (alreadyIn) break;
+                const attemptedCrossword = crossWord.replace('*', rackLetter.toLowerCase());
+                if (this.board.dictionnary.isValidWord(attemptedCrossword)) {
+                    validWords.push(this.deepCopyPlacementOption(option, option.word.replace(CONTACT_CHAR, rackLetter)));
+                    possibleLetters += rackLetter;
+                }
+            }
+            const replacement = JSON.parse(JSON.stringify(option));
+            replacement.word = possibleLetters;
+            exploredOptions.push(replacement);
+        }
+        return validWords;
+    }
+
+    private deepCopyPlacementOption(placement: PlacementOption, newWord?: string): PlacementOption {
+        return {row: placement.row, col: placement.col, isHorizontal: placement.isHorizontal, word: newWord ? newWord : placement.word};
+    }
+
+    private rackToString(): string {
+        let string = '';
+        for (const letter of this.game.reserve.letterRacks[AI_GAME_INDEX]) {
+            string += letter.name;
+        }
+        return string;
     }
 }
