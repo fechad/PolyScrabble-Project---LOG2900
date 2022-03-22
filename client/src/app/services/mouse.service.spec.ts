b0b2d373fedd0a6afdbf78028ccb0ffa1729d032
annagx <anna.guo@polymtl.ca>
2022-03-22 01:27:26 -0400

    mouse service tests

--- client/src/app/services/mouse.service.spec.ts
+++ client/src/app/services/mouse.service.spec.ts
@@ -1,11 +1,20 @@
 /* eslint-disable prettier/prettier */
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { Vec2 } from '@app/classes/vec2';
 import { MouseButton } from '@app/constants';
 import { from } from 'rxjs';
 import { GameContextService } from './game-context.service';
 import { GridService } from './grid.service';
 import { MouseService } from './mouse.service';
+
+const fakeIsInBound = (event: MouseEvent) => {
+    const size = 500;
+    const origin = 20;
+    const GRID_BORDERS = [origin, size];
+    return (
+        event.offsetX >= GRID_BORDERS[0] && event.offsetX <= GRID_BORDERS[1] && event.offsetY >= GRID_BORDERS[0] && event.offsetY <= GRID_BORDERS[1]
+    );
+};
 
 describe('MouseDetect', () => {
     let service: MouseService;
@@ -16,7 +25,7 @@ describe('MouseDetect', () => {
     beforeEach(() => {
         gameContextService = jasmine.createSpyObj('GameContextService', ['isMyTurn']);
         gameContextService.isMyTurn.and.callFake(() => from([true]));
-        gridService = jasmine.createSpyObj('GridService', ['drawGrid']);
+        gridService = jasmine.createSpyObj('GridService', ['drawGrid', 'drawArrow']);
         TestBed.configureTestingModule({
             providers: [
                 { provide: GameContextService, useValue: gameContextService },
@@ -24,17 +33,18 @@ describe('MouseDetect', () => {
             ],
         });
         service = TestBed.inject(MouseService);
-        // eslint-disable-next-line dot-notation -- getting private property
+        // to access the private attribute for this test file.
+        // eslint-disable-next-line dot-notation
         service['gridService'].letterWritten = 0;
     });
 
-    /* eslint-disable @typescript-eslint/no-magic-numbers */
+    /* eslint-disable @typescript-eslint/no-magic-numbers -- Add reason */
     it('mouseHitDetect should not change the mouse position if it is not a left click', () => {
         const expectedPosition: Vec2 = { x: 0, y: 0 };
         mouseEvent = {
             offsetX: expectedPosition.x + 10,
             offsetY: expectedPosition.y + 10,
-            button: 1,
+            button: MouseButton.Right,
         } as MouseEvent;
         service.mouseHitDetect(mouseEvent);
         expect(service.mousePosition).not.toEqual({ x: mouseEvent.offsetX, y: mouseEvent.offsetY });
@@ -72,6 +82,7 @@ describe('MouseDetect', () => {
     });
 
     it('isInBound should return false if positionX is under 20', async () => {
+        spyOn(service, 'isInBound').and.callFake(fakeIsInBound);
         mouseEvent = {
             offsetX: Math.random() * 19,
             offsetY: 20,
@@ -80,4 +91,71 @@ describe('MouseDetect', () => {
         const result = service.isInBound(mouseEvent);
         expect(result).toEqual(false);
     });
+
+    it('isInBound should return false if positionY is under 20', async () => {
+        spyOn(service, 'isInBound').and.callFake(fakeIsInBound);
+        mouseEvent = {
+            offsetX: 20,
+            offsetY: Math.random() * 19,
+            button: MouseButton.Right,
+        } as MouseEvent;
+        const result = service.isInBound(mouseEvent);
+        expect(result).toEqual(false);
+    });
+
+    it('isInBound should return false if positionX is over 500', async () => {
+        spyOn(service, 'isInBound').and.callFake(fakeIsInBound);
+        mouseEvent = {
+            offsetX: Math.random() * 501 + 500,
+            offsetY: 20,
+            button: MouseButton.Right,
+        } as MouseEvent;
+        const result = service.isInBound(mouseEvent);
+        expect(result).toEqual(false);
+    });
+
+    it('isInBound should return false if positionY is over 500', async () => {
+        spyOn(service, 'isInBound').and.callFake(fakeIsInBound);
+        mouseEvent = {
+            offsetX: 20,
+            offsetY: Math.random() * 501 + 500,
+            button: MouseButton.Right,
+        } as MouseEvent;
+        const result = service.isInBound(mouseEvent);
+        expect(result).toEqual(false);
+    });
+
+    it('isInBound should return true if position between is over (20,20) and (500,500)', async () => {
+        spyOn(service, 'isInBound').and.callFake(fakeIsInBound);
+        mouseEvent = {
+            offsetX: Math.floor(Math.random() * (500 - 20) + 20),
+            offsetY: Math.floor(Math.random() * (500 - 20) + 20),
+            button: MouseButton.Right,
+        } as MouseEvent;
+        const result = service.isInBound(mouseEvent);
+        expect(result).toEqual(true);
+    });
+
+    it('should call drawGrid on valid horizontal click', fakeAsync(() => {
+        spyOn(service, 'isInBound').and.callFake(fakeIsInBound);
+        mouseEvent = {
+            offsetX: Math.floor(Math.random() * (500 - 20) + 20),
+            offsetY: Math.floor(Math.random() * (500 - 20) + 20),
+            button: MouseButton.Left,
+        } as MouseEvent;
+        service.mouseHitDetect(mouseEvent);
+        tick();
+        expect(gridService.drawGrid).toHaveBeenCalled();
+    }));
+
+    it('should not call drawGrid on left click but  invalid mouse position ', fakeAsync(() => {
+        mouseEvent = {
+            offsetX: 0,
+            offsetY: 0,
+            button: MouseButton.Right,
+        } as MouseEvent;
+        const result = service.isInBound(mouseEvent);
+        tick();
+        expect(result).toBeFalse();
+    }));
 });
