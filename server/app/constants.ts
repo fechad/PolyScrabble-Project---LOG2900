07f66181f42899499fc8d8105d56fe2a40bf5486
francois-tourigny <francois.tourigny@polymtl.ca>
2022-03-22 23:04:48 -0400

    ajustements JV

--- server/app/constants.ts
+++ server/app/constants.ts
@@ -5,6 +5,7 @@ export const PROBABILITY = 10;
 export const BOARD_LENGTH = 15;
 export const CONTACT_CHAR = '#';
 export const DELAY_CHECK_TURN = 1000; // ms
+export const DELAY_NO_PLACEMENT = 20000; // ms
 export const HALF_PROBABILITY = 0.5;
 export const ROW_CONTACT = 0;
 export const COL_CONTACT = 1;
