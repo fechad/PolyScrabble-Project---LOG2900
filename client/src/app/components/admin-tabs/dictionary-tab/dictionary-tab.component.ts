f659f5684e6b0b301602da95ef3250c79c75b6e3
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-03-28 22:12:12 -0400

    finished html and csss for dictionnary tab content

--- client/src/app/components/admin-tabs/dictionary-tab/dictionary-tab.component.ts
+++ client/src/app/components/admin-tabs/dictionary-tab/dictionary-tab.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-dictionary-tab',
-  templateUrl: './dictionary-tab.component.html',
-  styleUrls: ['./dictionary-tab.component.scss']
+    selector: 'app-dictionary-tab',
+    templateUrl: './dictionary-tab.component.html',
+    styleUrls: ['./dictionary-tab.component.scss'],
 })
 export class DictionaryTabComponent implements OnInit {
+    faTrash = faTrashAlt;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
