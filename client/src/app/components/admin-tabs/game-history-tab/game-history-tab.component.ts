fa8d596c0e852c6b26c177af1edda2f8eff3551c
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-03-28 22:36:34 -0400

    finished history tab

--- client/src/app/components/admin-tabs/game-history-tab/game-history-tab.component.ts
+++ client/src/app/components/admin-tabs/game-history-tab/game-history-tab.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-game-history-tab',
-  templateUrl: './game-history-tab.component.html',
-  styleUrls: ['./game-history-tab.component.scss']
+    selector: 'app-game-history-tab',
+    templateUrl: './game-history-tab.component.html',
+    styleUrls: ['./game-history-tab.component.scss'],
 })
 export class GameHistoryTabComponent implements OnInit {
+    faTrash = faTrashAlt;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
