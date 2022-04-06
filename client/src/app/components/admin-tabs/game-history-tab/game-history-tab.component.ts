87ae60e52b2af3e243b5b2054ee4b84de2f02e25
Justin St-Arnaud <justin.st-arnaud@polymtl.ca>
2022-04-05 22:49:12 -0400

    connect data to game history tab component

--- client/src/app/components/admin-tabs/game-history-tab/game-history-tab.component.ts
+++ client/src/app/components/admin-tabs/game-history-tab/game-history-tab.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { GameHistory } from '@app/game-history';
 import { faSync, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-game-history-tab',
     templateUrl: './game-history-tab.component.html',
     styleUrls: ['./game-history-tab.component.scss'],
 })
-export class GameHistoryTabComponent {
+export class GameHistoryTabComponent implements OnInit {
     faTrash = faTrashAlt;
     faRefresh = faSync;
+    games: GameHistory[];
+    constructor(readonly httpClient: HttpClient) {}
+
+    async ngOnInit(): Promise<void> {
+        this.games = await this.httpClient.get<GameHistory[]>(`${environment.serverUrl}/game-history`).toPromise();
+    }
 }
