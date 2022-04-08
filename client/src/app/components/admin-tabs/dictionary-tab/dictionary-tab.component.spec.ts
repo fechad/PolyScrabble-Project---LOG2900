349d905a873db33f9ac98250daab74f3d43e8a27
annagx <anna.guo@polymtl.ca>
2022-04-08 11:10:35 -0400

    rebase with admin-logic for tests

--- client/src/app/components/admin-tabs/dictionary-tab/dictionary-tab.component.spec.ts
+++ client/src/app/components/admin-tabs/dictionary-tab/dictionary-tab.component.spec.ts
@@ -1,23 +1,109 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DictionaryTabComponent } from './dictionary-tab.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { DbDictionary, DictionaryTabComponent } from './dictionary-tab.component';
+
+const setHTML = () => {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    const input = document.createElement('input');
+    input.id = 'upload';
+    input.type = 'file';
+    input.accept = '.json';
+    input.classList.add('form-control');
+    div.appendChild(input);
+    document.body.appendChild(div);
+};
+
+const TIME_OUT = 10;
 
 describe('DictionaryTabComponent', () => {
     let component: DictionaryTabComponent;
     let fixture: ComponentFixture<DictionaryTabComponent>;
+    let httpMock: HttpTestingController;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, ReactiveFormsModule],
             declarations: [DictionaryTabComponent],
+            providers: [FormBuilder],
         }).compileComponents();
+        httpMock = TestBed.inject(HttpTestingController);
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(DictionaryTabComponent);
         component = fixture.componentInstance;
+        component.dictionaryForm = new FormGroup({
+            id: new FormControl(''),
+            title: new FormControl('', [Validators.pattern('^[A-zÀ-ù]*$')]),
+            description: new FormControl(''),
+            file: new FormControl('', [Validators.required]),
+        });
+        component.ngOnInit();
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should update list ont init', () => {
+        const updateListSpy = spyOn(component, 'updateList').and.callThrough();
+        component.ngOnInit();
+        expect(updateListSpy).toHaveBeenCalled();
+    });
+
+    it('should update list', fakeAsync(() => {
+        const list: DbDictionary[] = [
+            { id: 0, title: 'dict-0', description: 'test-0', words: ['a', 'b', 'c'] },
+            { id: 1, title: 'dict-1', description: 'test-1', words: ['d', 'e', 'f'] },
+        ];
+
+        const subscription = component.updateList();
+
+        from(subscription).subscribe(() => {
+            expect(component.list).toEqual(list);
+        });
+
+        const req = httpMock.match(`${environment.serverUrl}/dictionaries`);
+
+        expect(req[1].request.method).toBe('GET');
+        req[1].flush(list);
+
+        httpMock.verify();
+    }));
+
+    it('should create new dictionary', () => {
+        component.list = [{ id: 0, title: 'dict-0', description: 'test-0', words: ['a', 'b', 'c'] }];
+        const newDictionary: DbDictionary = { id: 1, title: 'dict-1', description: 'test-1', words: ['a', 'b', 'c'] };
+        const result: DbDictionary = component.createNewDbDictionary('dict-1', 'test-1', ['a', 'b', 'c']);
+        expect(result).toEqual(newDictionary);
+    });
+
+    it('should get file infos ', async () => {
+        setHTML();
+        component.list = [{ id: 0, title: 'dict-0', description: 'test-0', words: ['a', 'b', 'c'] }];
+        component.uploading = true;
+        const dataTransfer = new DataTransfer();
+        const expected: DbDictionary = { id: 1, title: 'dict-1', description: 'test-1', words: ['a', 'b', 'c'] };
+        const dictionaryFile = new File([JSON.stringify({ title: 'dict-1', description: 'test-1', words: ['a', 'b', 'c'] })], 'dict-1.json', {
+            type: 'application/json',
+        });
+        dataTransfer.items.add(dictionaryFile);
+        const inputCollection = document.getElementsByClassName('form-control');
+        const input = inputCollection.item(0) as HTMLInputElement;
+        input.files = dataTransfer.files;
+
+        input.addEventListener('change', async (event: Event) => await component.getFileInfos(event));
+        input.dispatchEvent(new InputEvent('change'));
+        await new Promise<void>((resolve) => {
+            setTimeout(() => resolve(), TIME_OUT);
+        });
+
+        expect(component.newDictionnary).toEqual(expected);
+        fixture.detectChanges();
     });
 });
--- client/src/app/components/admin-tabs/virtual-players-tab/virtual-players-tab.component.spec.ts
+++ client/src/app/components/admin-tabs/virtual-players-tab/virtual-players-tab.component.spec.ts
@@ -3,31 +3,16 @@ import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { from } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { VirtualPlayersTabComponent, VP } from './virtual-players-tab.component';
-// class HttpClientMock {
-//     list: VP[] = [
-//         { default: true, beginner: true, name: 'Antoine' },
-//         { default: false, beginner: true, name: 'Jean' },
-//         { default: true, beginner: false, name: 'Simone' },
-//         { default: false, beginner: false, name: 'Alexandra' },
-//     ];
-
-//     async get(url: string): Promise<VP[]> {
-//         if (url) return this.list;
-//         return [];
-//     }
-// }
 
 describe('VirtualPlayersTabComponent', () => {
     let component: VirtualPlayersTabComponent;
     let fixture: ComponentFixture<VirtualPlayersTabComponent>;
     let httpMock: HttpTestingController;
-    // let httpClient: HttpClient;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
             declarations: [VirtualPlayersTabComponent],
-            // providers: [{ provide: HttpClient, useValue: httpClient }],
         }).compileComponents();
         httpMock = TestBed.inject(HttpTestingController);
     });
@@ -38,94 +23,91 @@ describe('VirtualPlayersTabComponent', () => {
         fixture.detectChanges();
     });
 
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
-    // });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-    // it('should update list ont init', () => {
-    //     const updateListSpy = spyOn(component, 'updateList').and.callThrough();
-    //     component.ngOnInit();
-    //     expect(updateListSpy).toHaveBeenCalled();
-    // });
+    it('should update list ont init', () => {
+        const updateListSpy = spyOn(component, 'updateList').and.callThrough();
+        component.ngOnInit();
+        expect(updateListSpy).toHaveBeenCalled();
+    });
 
-    // it('should update list', fakeAsync(() => {
-    //     const list: VP[] = [
-    //         { default: true, beginner: true, name: 'Antoine' },
-    //         { default: false, beginner: true, name: 'Jean' },
-    //         { default: true, beginner: false, name: 'Simone' },
-    //         { default: false, beginner: false, name: 'Alexandra' },
-    //     ];
+    it('should update list', fakeAsync(() => {
+        const list: VP[] = [
+            { default: true, beginner: true, name: 'Antoine' },
+            { default: false, beginner: true, name: 'Jean' },
+            { default: true, beginner: false, name: 'Simone' },
+            { default: false, beginner: false, name: 'Alexandra' },
+        ];
 
-    //     const subscription = component.updateList();
+        const subscription = component.updateList();
 
-    //     from(subscription).subscribe(() => {
-    //         expect(component.list).toEqual(list);
-    //         console.log('component list', component.list);
-    //     });
+        from(subscription).subscribe(() => {
+            expect(component.list).toEqual(list);
+        });
 
-    //     const req = httpMock.match(`${environment.serverUrl}/vp-names`);
+        const req = httpMock.match(`${environment.serverUrl}/vp-names`);
 
-    //     expect(req[1].request.method).toBe('GET');
-    //     req[1].flush(list);
+        expect(req[1].request.method).toBe('GET');
+        req[1].flush(list);
 
-    //     httpMock.verify();
-    // }));
+        httpMock.verify();
+    }));
 
-    // it('should add player to list', fakeAsync(() => {
-    //     const list: VP[] = [
-    //         { default: true, beginner: true, name: 'Antoine' },
-    //         { default: false, beginner: true, name: 'Jean' },
-    //         { default: true, beginner: false, name: 'Simone' },
-    //         { default: false, beginner: false, name: 'Alexandra' },
-    //         { default: false, beginner: true, name: 'Anna' },
-    //     ];
+    it('should add player to list', fakeAsync(() => {
+        const list: VP[] = [
+            { default: true, beginner: true, name: 'Antoine' },
+            { default: false, beginner: true, name: 'Jean' },
+            { default: true, beginner: false, name: 'Simone' },
+            { default: false, beginner: false, name: 'Alexandra' },
+            { default: false, beginner: true, name: 'Anna' },
+        ];
 
-    //     const beginnerList: VP[] = [
-    //         { default: true, beginner: true, name: 'Antoine' },
-    //         { default: false, beginner: true, name: 'Jean' },
-    //         { default: false, beginner: true, name: 'Anna' },
-    //     ];
+        const beginnerList: VP[] = [
+            { default: true, beginner: true, name: 'Antoine' },
+            { default: false, beginner: true, name: 'Jean' },
+            { default: false, beginner: true, name: 'Anna' },
+        ];
 
-    //     const newVp: VP = { default: false, beginner: true, name: 'Anna' };
+        const newVp: VP = { default: false, beginner: true, name: 'Anna' };
 
-    //     const subscription = component.addPlayer(newVp.name, newVp.beginner);
+        const subscription = component.addPlayer(newVp.name, newVp.beginner);
 
-    //     from(subscription).subscribe(() => {
-    //         expect(component.beginnerList).toEqual(beginnerList);
-    //         console.log('beginner list', component.beginnerList);
-    //     });
+        from(subscription).subscribe(() => {
+            expect(component.beginnerList).toEqual(beginnerList);
+        });
 
-    //     const req = httpMock.match(`${environment.serverUrl}/vp-names`);
-    //     expect(req[1].request.method).toBe('POST');
-    //     req[1].flush(list);
-    //     req[0].flush(list);
+        const req = httpMock.match(`${environment.serverUrl}/vp-names`);
+        expect(req[1].request.method).toBe('POST');
+        req[1].flush(list);
+        req[0].flush(list);
 
-    //     httpMock.verify();
-    // }));
-    // it('should update player in list', fakeAsync(() => {
-    //     const list: VP[] = [
-    //         { default: true, beginner: true, name: 'Anna' },
-    //         { default: false, beginner: true, name: 'Jean' },
-    //         { default: true, beginner: false, name: 'Simone' },
-    //         { default: false, beginner: false, name: 'Alexandra' },
-    //     ];
+        httpMock.verify();
+    }));
+    it('should update player in list', fakeAsync(() => {
+        const list: VP[] = [
+            { default: true, beginner: true, name: 'Anna' },
+            { default: false, beginner: true, name: 'Jean' },
+            { default: true, beginner: false, name: 'Simone' },
+            { default: false, beginner: false, name: 'Alexandra' },
+        ];
 
-    //     const newVp: VP = { default: true, beginner: true, name: 'Anna' };
+        const newVp: VP = { default: true, beginner: true, name: 'Anna' };
 
-    //     const subscription = component.updatePlayer('Antoine', newVp.name, newVp.beginner);
+        const subscription = component.updatePlayer('Antoine', newVp.name, newVp.beginner);
 
-    //     from(subscription).subscribe(() => {
-    //         expect(component.list[0]).toEqual(newVp);
-    //         console.log('new VP', component.beginnerList[0]);
-    //     });
+        from(subscription).subscribe(() => {
+            expect(component.list[0]).toEqual(newVp);
+        });
 
-    //     const req = httpMock.match(`${environment.serverUrl}/vp-names`);
-    //     expect(req[1].request.method).toBe('PATCH');
-    //     req[1].flush(list);
-    //     req[0].flush(list);
+        const req = httpMock.match(`${environment.serverUrl}/vp-names`);
+        expect(req[1].request.method).toBe('PATCH');
+        req[1].flush(list);
+        req[0].flush(list);
 
-    //     httpMock.verify();
-    // }));
+        httpMock.verify();
+    }));
 
     it('should delete player from list', fakeAsync(() => {
         const result: VP[] = [
@@ -138,7 +120,6 @@ describe('VirtualPlayersTabComponent', () => {
 
         from(subscription).subscribe(() => {
             expect(component.list).toEqual(result);
-            console.log('new list', component.list);
         });
 
         const reqDelete = httpMock.match(`${environment.serverUrl}/vp-names/Anna`);
@@ -152,9 +133,45 @@ describe('VirtualPlayersTabComponent', () => {
         httpMock.verify();
     }));
 
-    it('should update list ont init', () => {
-        const updateListSpy = spyOn(component, 'updateList').and.callThrough();
-        component.ngOnInit();
-        expect(updateListSpy).toHaveBeenCalled();
+    it('should validate good name', () => {
+        const validName: string[] = ['ama', 'anana', 'Anno'];
+        const result1 = component.invalidName(validName[0], true);
+        const result2 = component.invalidName(validName[1], true);
+        const result3 = component.invalidName(validName[2], true);
+        expect(result1).toBe(false);
+        expect(result2).toBe(false);
+        expect(result3).toBe(false);
+    });
+
+    it('should invalidate bad names', () => {
+        const invalidName: string[] = ['123', 'V-/', '*+'];
+        const result1 = component.invalidName(invalidName[0], true);
+        const result2 = component.invalidName(invalidName[1], true);
+        const result3 = component.invalidName(invalidName[2], true);
+        expect(result1).toBe(true);
+        expect(result2).toBe(true);
+        expect(result3).toBe(true);
+    });
+
+    it('should tell if name already in list', () => {
+        const list: VP[] = [
+            { default: false, beginner: true, name: 'Jean' },
+            { default: true, beginner: false, name: 'Simone' },
+            { default: false, beginner: false, name: 'Alexandra' },
+        ];
+
+        component.list = list;
+        const result: boolean = component.findDoubles('Simone', false);
+        expect(result).toBe(true);
+    });
+
+    it('should hideInput', () => {
+        component.nameInput = 'test1';
+        component.nameInputExpert = 'test2';
+        component.hideInput(true);
+        component.hideInput(false);
+        expect(component.nameInput).toBe('');
+        expect(component.nameInputExpert).toBe('');
+        expect(component.clicked).toEqual([false, false]);
     });
 });
