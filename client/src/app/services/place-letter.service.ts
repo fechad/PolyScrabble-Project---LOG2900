be0daeea503c68198a78905ef8a17f8029c6ec71
Etienne123456 <etienneaumais@gmail.com>
2022-04-19 15:55:31 -0400

    fixed the out of bound bug

--- client/src/app/services/place-letter.service.ts
+++ client/src/app/services/place-letter.service.ts
@@ -81,6 +81,7 @@ export class PlaceLetterService {
             this.mouseDetectService.isHorizontal,
         );
         const lastLetter = this.gridService.letterPosition[this.gridService.letterPosition.length - 1];
+        this.nextPosExist();
         if (this.nextPosExist()) this.drawShiftedArrow(lastLetter, this.getShift(lastLetter));
         this.gameContextService.tempUpdateRack();
     }
@@ -103,10 +104,10 @@ export class PlaceLetterService {
         const board = this.gameContextService.state.value.board;
         let shift = 2;
         const horizontal = this.mouseDetectService.isHorizontal;
-        let x = horizontal ? pos.x : pos.x + 1;
-        let y = horizontal ? pos.y + 1 : pos.y;
-        while (y !== constants.BOARD_SIZE && board[x][y]) {
-            if (horizontal) x++;
+        let x = !horizontal ? pos.x + 1 : pos.x;
+        let y = !horizontal ? pos.y : pos.y + 1;
+        while (y !== constants.BOARD_SIZE && x !== constants.BOARD_SIZE && board[x][y]) {
+            if (!horizontal) x++;
             else y++;
             shift++;
         }
